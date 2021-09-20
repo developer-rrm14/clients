b8fa9d8a9f3d57982eb0f15a05f7ed2cb1ef94ea
moduretick <robertoreboucasdemelo@gmail.com>
2021-09-20 13:17:41 -0300

    first commit

---
+++ src/main/java/com/rrm14/clientsapp/ClientsAppApplication.java
@@ -0,0 +1,13 @@
+package com.rrm14.clientsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rrm14/clientsapp/ClientsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrm14.clientsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
